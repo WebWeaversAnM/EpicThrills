0fcbae4e01d29bb122848d3fe792ba06f7af5edc
Mathew Gillroy <mathew1496@outlook.com>
2017-09-02 20:26:09 +0530

    Navbar Modification

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,9 @@
-var a = $(".navbar-default").offset().top;
-
-        $(document).scroll(function(){
-            if($(this).scrollTop() > a)
-            {   
-               $('.navbar-default').css({"background-color":"#645E62","border-color":"transparent"});
-            } else {
-               $('.navbar-default').css({"background-color":"transparent","border-color":"transparent"});
-            }
+var a = $('.navbar-default').offset().top;
+$(document).scroll(function(){
+  if($(this).scrollTop()>a){
+    $('.navbar-default').css({"background-color":"#A19A8A","border-color":"#F0E8DB","border-width":"2px"});
+  }
+  else{
+    $('.navbar-default').css({"background-color":"transparent","border-color":"transparent"});
+  }
 });
