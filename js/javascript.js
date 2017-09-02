e9c31b4523d537210edf8236ef8e0513e57f5bae
Mathew Gillroy <mathew1496@outlook.com>
2017-09-02 20:54:12 +0530

    Color

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,11 @@
 var a = $('.navbar-default').offset().top;
 $(document).scroll(function(){
   if($(this).scrollTop()>a){
-    $('.navbar-default').css({"background-color":"#A19A8A","border-color":"#F0E8DB","border-width":"2px"});
+    $('.navbar-default').css({"background-color":"white","border-color":"#F0E8DB","border-width":"2px"});
+    $('#main-head').css({"color":"#524758"});
   }
   else{
-    $('.navbar-default').css({"background-color":"transparent","border-color":"transparent"});
+    $('.navbar-default').css({"background-color":"transparent","border-color":"transparent","border-width":"2px"});
+    $('#main-head').css({"color":"white"});
   }
 });
