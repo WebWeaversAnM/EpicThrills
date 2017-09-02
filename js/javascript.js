cf48c1d051faca562fee9dfec9d04cebccc92b5d
Mathew Gillroy <mathew1496@outlook.com>
2017-09-02 22:44:03 +0530

    Nav Colors

--- js/javascript.js
+++ js/javascript.js
@@ -3,9 +3,11 @@ $(document).scroll(function(){
   if($(this).scrollTop()>a){
     $('.navbar-default').css({"background-color":"white","border-color":"#F0E8DB","border-width":"2px"});
     $('#main-head').css({"color":"#524758"});
+    $('.navbar-default .navbar-nav > li > a').css({"color":"#524758"});
   }
   else{
     $('.navbar-default').css({"background-color":"transparent","border-color":"transparent","border-width":"2px"});
     $('#main-head').css({"color":"white"});
+    $('.navbar-default .navbar-nav > li > a').css({"color":"white"});
   }
 });
