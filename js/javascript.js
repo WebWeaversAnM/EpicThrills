f43176739289389133c68bb8ed3a4c10c44d6ec7
Mathew Gillroy <mathew1496@outlook.com>
2017-09-04 01:58:56 +0530

    Carousel Start

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,12 @@
 var a = $('.navbar-default').offset().top;
 $(document).scroll(function(){
   if($(this).scrollTop()>a){
-    $('.navbar-default').css({"background-color":"white","border-color":"#F0E8DB","border-width":"3px"});
+    $('.navbar-default').css({"background-color":"white","border-color":"#F0E8DB"});
     $('#main-head').css({"color":"#524758"});
     $('.navbar-default .navbar-nav > li > a').css({"color":"#524758"});
   }
   else{
-    $('.navbar-default').css({"background-color":"transparent","border-color":"transparent","border-width":"3px"});
+    $('.navbar-default').css({"background-color":"transparent","border-color":"transparent"});
     $('#main-head').css({"color":"white"});
     $('.navbar-default .navbar-nav > li > a').css({"color":"white"});
   }
